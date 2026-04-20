90f5fa9a69e9f2e015c0c6a65397b374a572edb5
Luke Frost <lukefrost96@hotmail.co.uk>
2026-04-20 15:07:33 +0100

    Added new slug field to the product model. Also changed the Product seeder to include the new slug field when seeding the database.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * Generates the slug for the new product before it is saved to the database
+     * @return void
+     */
+    protected static function booted(): void
+    {
+        static::creating(function (Product $product) {
+            if (empty($product->slug)) {
+                $product->slug = Str::slug($product->name);
+            }
+        });
+    }
+
+    /**
+     * Defines the route key name for model binding to use the 'slug' column.
+     * @return string
+     */
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
 }
