d0486f22522570a6ec24181ca507591181124ed2
Luke Frost <lukefrost96@hotmail.co.uk>
2026-04-20 18:59:41 +0100

    Updated ProductSeeder class to add images for the image gallery

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -20,6 +20,11 @@ class ProductSeeder extends Seeder
                 'description' => '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p><p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p><p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend</p>',
                 'image' => 'example-product-1.jpg',
                 'price' => 5,
+                'images' => [
+                    'example-product-1.jpg',
+                    'example-product-1-black.jpg',
+                    'example-product-1-white.jpg',
+                ],
             ]
         );
 
@@ -31,6 +36,14 @@ class ProductSeeder extends Seeder
                 'description' => '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p><p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p><p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend</p>',
                 'image' => 'example-product-2.jpg',
                 'price' => 2.50,
+                'images' => [
+                    'example-product-2.jpg',
+                    'example-product-2-black.jpg',
+                    'example-product-2-blue.jpg',
+                    'example-product-2-darkblue.jpg',
+                    'example-product-2-orange.jpg',
+                    'example-product-2-red.jpg',
+                ],
             ]
         );
 
@@ -42,6 +55,13 @@ class ProductSeeder extends Seeder
                 'description' => '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p><p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p><p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend</p>',
                 'image' => 'example-product-3.jpg',
                 'price' => 3.25,
+                'images' => [
+                    'example-product-3.jpg',
+                    'example-product-3-black.jpg',
+                    'example-product-3-blue.jpg',
+                    'example-product-3-grey.jpg',
+                    'example-product-3-red.jpg',
+                ],
             ]
         );
 
@@ -53,6 +73,11 @@ class ProductSeeder extends Seeder
                 'description' => '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p><p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p><p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend</p>',
                 'image' => 'example-product-4.jpg',
                 'price' => 7.50,
+                'images' => [
+                    'example-product-4.jpg',
+                    'example-product-4-black.jpg',
+                    'example-product-4-red.jpg',
+                ],
             ]
         );
     }
