fec5c30be94534d00a0f8dffdc97fcdd4ceff0b7
Luke Frost <lukefrost96@hotmail.co.uk>
2026-04-21 12:32:06 +0100

    Added Product enquiry method to the Product Controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,15 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SendEnquiryRequest;
+use App\Mail\ProductEnquiry;
 use App\Models\Product;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Mail;
 
 class ProductController extends Controller
 {
     /**
-     * Display the specified resource.
+     * Show Product details
+     * @param Product $product
+     * @return Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\View\View
      */
     public function show(Product $product)
     {
+        $product->load('images');
+
         return view('product.show', compact('product'));
+    }
+
+    /**
+     * Submit the enquiry for the product
+     * @param SendEnquiryRequest $request
+     * @param Product $product
+     * @return RedirectResponse
+     */
+    public function sendEnquiry(SendEnquiryRequest $request, Product $product): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        return back()->with('enquiry_sent', true);
     }
 }
